5025ea23ea23a8195f9a91bf5d4179f9928cd74a
HrinchenkoDmytro <d.greenchenko@gmail.com>
2016-02-22 12:01:03 +0200

    Initial commit

---
+++ src/epam/com/Main.java
@@ -0,0 +1,18 @@
+package epam.com;
+
+/**
+ * Created by Любовь on 22.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Object[] object1 = new Object []{"a","b","c","d","e"};
+
+
+        for (Object element : object1){
+            System.out.println(element);
+
+        }
+    }
+}
